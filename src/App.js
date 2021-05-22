477252616e361a036233debdce9b9c8cb2ca9735
Phu Vo <vongocphu04@gmail.com>
2021-05-22 15:32:56 +0700

    viet giao dien trang login

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {Register} from './screens';
+function App() {
+  return (
+    <div className="App">
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
